e7b56ae5f944f96c8bf2a31d2abbb75ef208fc0c
LAB_B_REAL <labb@continental.edu.pe>
2024-02-17 13:29:14 -0500

    Primera clase

--- PHP-I/index.php
+++ PHP-I/index.php
@@ -23,10 +23,10 @@ echo "El sistema operativo que tengo es: " . PHP_OS;
 
 /* Operadores */
 
-$numero = 1;
+/* $numero = 1;
 $a = 1;
 $b = 3;
-$numeroTexto = '1';
+$numeroTexto = '1'; */
 
 //var_dump($numero === $numeroTexto && $numero == 1);
 
@@ -42,5 +42,27 @@ $numeroTexto = '1';
 //$a++; #$a=5
 //$a = $a - $b;
 #$a -= $b;
-$b *= $a;
-echo $b;
+/* $b *= $a;
+echo $b; */
+
+/* Condicionales */
+
+/* $c = 1;
+$d = '1';
+
+if ($d > $c) {
+    $d++;
+    echo "El valor de d es  $d <br>";
+    echo "D es mayor a C";
+} elseif ($d === $c) {
+    echo "D es igual a C";
+} elseif ($c > $d) {
+    echo "C es mayor a D";
+} elseif ($c == $d) {
+    echo "C es igual a D";
+} */
+$c = 10;
+$d = 15;
+$cEsMayorAd = $c > $d ? 'c es mayor a d' : ($c === $d ? 'c es igual a d' : 'c es menor a d');
+
+echo $cEsMayorAd;
